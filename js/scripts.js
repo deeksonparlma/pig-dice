a997bdcfa439d2d8ebe37a8e9d1e19f73e8c5a75
Dickson <dicksonparlma@gmail.com>
2019-03-01 14:07:17 +0300

    function when player one rolls dice

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,10 @@ function player(firstName,secondName){
   this.firstName=firstName;
   this.secondName=secondName;
 }
+
 var dice1=[1,2,3,4,5,6];
 var dice2=[1,2,3,4,5,6];
+function roll1(){
+  var result1= dice1[Math.floor(Math.random() * 6)];
+  var result2=dice2[Math.floor(Math.random() * 6)];
+}

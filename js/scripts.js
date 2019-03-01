1eeb87573f2dfcd55a8e2dc99a00aef39ba4d21d
Dickson <dicksonparlma@gmail.com>
2019-03-01 14:21:19 +0300

    push the numbers generated to the start of the array

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,8 @@ var dice2=[1,2,3,4,5,6];
 function roll1(){
   var result1= dice1[Math.floor(Math.random() * 6)];
   var result2=dice2[Math.floor(Math.random() * 6)];
-  var store1=results.push(result1);
-  var store2=results.push(result2);
+  var store1=results.unshift(result1);
+  var store2=results.unshift(result2);
   for(var index=0;index<results.length;index+=1){
     console.log(results[index]+results[index+=1])
   }

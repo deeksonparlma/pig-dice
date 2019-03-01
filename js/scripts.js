7a27e790cc2d3cd7cd0d50ef47919658a59ccc3b
Dickson <dicksonparlma@gmail.com>
2019-03-01 22:46:19 +0300

    checks set right

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,34 @@
-//Player constructor//
+  //Player constructor//
 
-function player(firstName,secondName){
-  this.firstName=firstName;
-  this.secondName=secondName;
-}
-var results=[0];
-var res1=[0];
-var res2=[0];
-var dice1=[1,2,3,4,5,6];
-var dice2=[1,2,3,4,5,6];
-function roll1(){
-  var result1= dice1[Math.floor(Math.random() * 6)];
-  var result2=dice2[Math.floor(Math.random() * 6)];
-  var store1=res1.unshift(result1);
-  var store2=res2.unshift(result2);
-  var store3=results.unshift(result1);
-  var store4=results.unshift(result2);
-  console.log(result1);
-  console.log(result2);
-  
-}
-function check(){ 
-  var checkNow=results.includes(1);
-if(checkNow===true){
-  var sent=results.unshift(0);
-}
-else{
-  for(var index=0;index<=results.length;index+=1){
-    var total=start[0];
-    return res1[index]+res2[index];
+  function player(firstName,secondName){
+    this.firstName=firstName;
+    this.secondName=secondName;
   }
-} 
-console.log(results[0]);
+  var result=[];
+  
+  var dice1=[1,2,3,4,5,6];
+  var dice2=[1,2,3,4,5,6];
+
+  //player one//
+
+  function roll1(){
+    var result1=result.push(dice1[Math.floor(Math.random() * 6)]);
+    var result2=result.push(dice2[Math.floor(Math.random() * 6)]);
+    console.log(result)
+  }
+  function check(){ 
+    var checkNow=result.includes(1);
+       if(checkNow===true){
+        for (var i = 0, sum = 0; i < result.length; console.log(0));
+  }
+  else{
+    
+    for (var i = 0, sum = 0; i < result.length; console.log(sum += result[i++]));
 }
-function clear(){
-  console.log(results.length=0);
-}
+  }
+  function clear(){
+    console.log(results.length=0);
+  }
+  function addUp(){
+    alert(sumStorage[0]+sumStorage[1])
+  }

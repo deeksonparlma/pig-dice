9a45b642de090186089f87c91e4dcbf51c710dd7
Dickson <dicksonparlma@gmail.com>
2019-03-01 14:12:10 +0300

    push the generated numbers from roll to array

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,6 @@ var dice2=[1,2,3,4,5,6];
 function roll1(){
   var result1= dice1[Math.floor(Math.random() * 6)];
   var result2=dice2[Math.floor(Math.random() * 6)];
-  var store1=result1;
-  var store2=result2;
+  var store1=results.push(result1);
+  var store2=results.push(result2);
 }

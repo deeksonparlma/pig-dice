3732cc62f526672f05bb51fe111973bf8928d52c
Dickson <dicksonparlma@gmail.com>
2019-03-02 10:27:48 +0300

    Add numbers from the rolled dice which are stored in an array

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,35 @@
     this.secondName=secondName;
   }
   var result=[];
-  
+  var liveScores=[0];
+  var totals=[0,0];
   var dice1=[1,2,3,4,5,6];
   var dice2=[1,2,3,4,5,6];
 
   //player one//
 
   function roll1(){
-    var result1=result.push(dice1[Math.floor(Math.random() * 6)]);
-    var result2=result.push(dice2[Math.floor(Math.random() * 6)]);
-    console.log(result)
+    var result1=result.unshift(dice1[Math.floor(Math.random() * 6)]);
+    var result2=result.unshift(dice2[Math.floor(Math.random() * 6)]);
+    var rollNumbers=result;
+    document.getElementById("playerRolls").innerHTML=rollNumbers;
   }
-  function check(){ 
+  function check(){
+    var fail=0;
     var checkNow=result.includes(1);
-       if(checkNow===true){
-        for (var i = 0, sum = 0; i < result.length; console.log(0));
+       if(checkNow===false){
+        for (var i = 0, sum = 0; i < result.length;
+          totals.unshift(sum += result[i++]));
+          document.getElementById("playerScore").innerHTML=totals[0]+ 0;
+          document.getElementById("playerLiveScore").innerHTML=totals[0];
+          liveScores.unshift(totals[0]);
   }
   else{
-    
-    for (var i = 0, sum = 0; i < result.length; console.log(sum += result[i++]));
-}
+    totals.unshift(fail);
+    document.getElementById("playerScore").innerHTML=totals[0];
+    document.getElementById("playerLiveScore").innerHTML=totals[0] + totals[2];
+    }
   }
   function clear(){
-    console.log(results.length=0);
-  }
-  function addUp(){
-    alert(sumStorage[0]+sumStorage[1])
+    document.getElementById("playerRolls").innerHTML=result[lastindex];
   }
